fc3edd7f3a20b7027324084559352905b947ff59
fernandomg <fernando.greco@gmail.com>
2018-09-13 16:59:54 -0300

    ContractStore tests

--- src/stores/ContractStore.js
+++ src/stores/ContractStore.js
@@ -1,24 +1,24 @@
-import { observable, action } from 'mobx'
+import { action, isObservable, observable } from 'mobx'
 import autosave from './autosave'
 
 class ContractStore {
-  @observable MintedCappedProxy
-  @observable DutchProxy
-  @observable ProxiesRegistry
-  @observable abstractStorage
-  @observable registryIdx
-  @observable provider
-  @observable registryExec
-  @observable idxMintedCapped
-  @observable saleMintedCapped
-  @observable saleManagerMintedCapped
-  @observable tokenMintedCapped
-  @observable tokenManagerMintedCapped
-  @observable idxDutch
-  @observable saleDutch
-  @observable tokenDutch
-  @observable crowdsale
-  @observable downloadStatus
+  @observable MintedCappedProxy = {}
+  @observable DutchProxy = {}
+  @observable ProxiesRegistry = {}
+  @observable abstractStorage = {}
+  @observable registryIdx = {}
+  @observable provider = {}
+  @observable registryExec = {}
+  @observable idxMintedCapped = {}
+  @observable saleMintedCapped = {}
+  @observable saleManagerMintedCapped = {}
+  @observable tokenMintedCapped = {}
+  @observable tokenManagerMintedCapped = {}
+  @observable idxDutch = {}
+  @observable saleDutch = {}
+  @observable tokenDutch = {}
+  @observable crowdsale = {}
+  @observable downloadStatus = {}
 
   constructor() {
     this.reset()
@@ -27,6 +27,10 @@ class ContractStore {
 
   @action
   setContract = (contractName, contractObj) => {
+    if (!isObservable(this[contractName])) {
+      // A contract must be declared as an observable to have it properly stored in browser's localStorage
+      throw new Error(`${contractName} is not declared as an observable property`)
+    }
     this[contractName] = contractObj
   }
 
@@ -44,23 +48,23 @@ class ContractStore {
 
   @action
   reset = () => {
-    this.MintedCappedProxy = undefined
-    this.DutchProxy = undefined
-    this.ProxiesRegistry = undefined
-    this.abstractStorage = undefined
-    this.registryIdx = undefined
-    this.provider = undefined
-    this.registryExec = undefined
-    this.idxMintedCapped = undefined
-    this.saleMintedCapped = undefined
-    this.saleManagerMintedCapped = undefined
-    this.tokenMintedCapped = undefined
-    this.tokenManagerMintedCapped = undefined
-    this.idxDutch = undefined
-    this.saleDutch = undefined
-    this.tokenDutch = undefined
-    this.crowdsale = undefined
-    this.downloadStatus = undefined
+    this.MintedCappedProxy = {}
+    this.DutchProxy = {}
+    this.ProxiesRegistry = {}
+    this.abstractStorage = {}
+    this.registryIdx = {}
+    this.provider = {}
+    this.registryExec = {}
+    this.idxMintedCapped = {}
+    this.saleMintedCapped = {}
+    this.saleManagerMintedCapped = {}
+    this.tokenMintedCapped = {}
+    this.tokenManagerMintedCapped = {}
+    this.idxDutch = {}
+    this.saleDutch = {}
+    this.tokenDutch = {}
+    this.crowdsale = {}
+    this.downloadStatus = {}
   }
 }
 
